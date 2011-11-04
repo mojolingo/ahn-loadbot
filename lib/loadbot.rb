59fad37e861b96c154e38cea53dd2e33a9b4b5ea
Bryan Rite <brite@mojolingo.com>
2011-11-04 13:03:08 -0700

    working bot, handles complex entries, addition of answer specific delays

--- lib/loadbot.rb
+++ lib/loadbot.rb
@@ -8,11 +8,8 @@ methods_for :rpc do
   def bot_call(plan_name)
     ahn_log.loadbot.debug COMPONENTS.loadbot.inspect
     config = COMPONENTS.loadbot['config']
-    plan = COMPONENTS.loadbot['plans'][plan_name]
+    plan = LoadBot.load_plan(plan_name)
     
-    raise ArgumentError, "No plan '#{plan_name}' exists in the loadbot config file" if plan.nil?
-    # TODO: Add a check to ensure the number exists in the plan.
-
     manager = Adhearsion::VoIP::Asterisk.manager_interface
     manager.call_and_exec "#{config['prefix']}/#{plan['number']}", "agi", :args => "agi://#{config['agi_server']}/loadbot", :variables => {:plan => plan_name}
   end
@@ -22,28 +19,45 @@ class LoadBot
   def initialize(call, plan_name)
     ahn_log.loadbot.debug "Initializing LoadBot with plan: #{plan_name}"
     @call = call
-    # TODO: Add a check to ensure plan exists and there is an answer array.
-    @plan = COMPONENTS.loadbot['plans'][plan_name]
-    @plan['name'] = plan_name
+    @plan = load_plan(plan_name)
   end
 
   def run
     ahn_log.loadbot.info "Running LoadBot plan '#{@plan['name']}"
 
+    # TODO: put the average wait in the config file, remember, the
+    # a per answer wait time will be added to wait if specified.
+    wait = 2 # seconds
+
     # As it usually takes a second for an initial recording to kick in,
-    # we wait a couple seconds before starting our test plan.
-    sleep(2.seconds)
+    # we wait before starting our test plan.
+    sleep(wait.seconds)
 
     # For each answer sequence we go through and submit requests
     # properly.
     @plan['answers'].each do |a|
       @call.execute 'WaitForSilence'
-      ahn_log.loadbot.debug "Submitting answer: #{a}"
-      @call.execute 'SendDTMF', a
-      sleep(2.seconds)
+      answer,delay = a.to_s().split(',')
+      delay = delay.nil? ? 0 : delay.to_i()
+      ahn_log.loadbot.debug "Answer: #{answer} : Delay: #{delay} second(s)"
+      @call.execute 'SendDTMF', answer unless answer.downcase.eql? "sleep"
+      sleep((wait + delay).seconds)
     end
 
     @call.execute 'WaitForSilence'
+
+  end
+
+  def self.load_plan(plan_name)
+    plan = COMPONENTS.loadbot['plans'][plan_name]
+
+    raise ArgumentError, "No plan '#{plan_name} exists in the loadbot config file" if plan.nil?
+
+    plan['name'] = plan_name
+
+    # TODO: do some more sanity checks in here:
+    # Does the plan have a number?
+    # Does it have some answers? etc.
 
   end
 end
